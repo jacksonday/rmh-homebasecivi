afcc995ee95a73eb6874aaf1bd4129914ab6a455
allen <allen@bowdoin.edu>
2013-12-05 13:16:26 -0500

    added screenshots for tutorial pages

---
+++ tutorial/navigateThroughWeeksHelp.inc.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Copyright 2013 by Jerrick Hoang, Ivy Xing, Sam Roberts, James Cook, 
+ * Johnny Coster, Judy Yang, Jackson Moniaga, Oliver Radwan, 
+ * Maxwell Palmer, Nolan McNair, Taylor Talmage, and Allen Tucker. 
+ * This program is part of RMH Homebase, which is free software.  It comes with 
+ * absolutely no warranty. You can redistribute and/or modify it under the terms 
+ * of the GNU General Public License as published by the Free Software Foundation
+ * (see <http://www.gnu.org/licenses/ for more information).
+ * 
+ */
+?>
+
+
+	<p> <strong>Navigate to Different Weeks</strong>
+	<p> Click the <b>Next Week</b> or <b>Previous Week</b> to navigate to different weeks:
+	<br><br><a href="tutorial/screenshots/navigateThroughWeeksHelp_navigate_weeks.png" class="image" title="navigateThroughWeeksHelp_navigate_weeks.png"
+			horizontalalign="center" target="tutorial/screenshots/navigateThroughWeeksHelp_navigate_weeks.png">
+		 &nbsp&nbsp&nbsp&nbsp<img src="tutorial/screenshots/navigateThroughWeeksHelp_navigate_weeks.png" width="10%" border="1px" align="center"></a>
+		 &nbsp&nbsp&nbsp&nbsp<img src="tutorial/screenshots/navigateThroughWeeksHelp_navigate_weeks.png" width="10%" border="1px" align="center"></a>
